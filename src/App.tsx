3d25d0e243ef588fc5ea8a659756378be82e0719
CCMatson <catherine.matson@gmail.com>
2023-03-26 20:16:28 -0700

    center the div

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <main>
+        <section>
+        <h1>Weather Forecast</h1>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Enter a city to view the weather:
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div>
+          <input type="text"/>
+          <button>
+            Search
+          </button>
+        </div>
+        </section>
+      </main>
+
     </div>
   );
 }
