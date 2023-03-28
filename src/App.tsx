4e8fb0e10e09f76ee084b37eb12d9fa816371d71
CCMatson <catherine.matson@gmail.com>
2023-03-28 12:20:48 -0700

    add getSearch function to fetch 5 pieces of data from api for each search form

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,24 @@ import './App.css';
 
 const App = (): JSX.Element => {
 
-  const [form, setForm] = useState('')
+  const [form, setForm] = useState<string>('')
 
-  const onInputChange = (e: ChangeEvent<HTMLInputElement> ) => {
-    setForm(e.target.value)
-
+  const getSearch = (value: string) => {
+    fetch(`  http://api.openweathermap.org/geo/1.0/direct?q=${value.trim()}&limit=5&appid=${process.env.REACT_APP_API_KEY}`)
+    // https://api.openweathermap.org/data/2.5/weather?q=${value.trim()}&appid=${process.env.REACT_APP_API_KEY}`)
+    .then((res) => res.json())
+    .then((data) => console.log({data}))
   }
 
-  // https://api.openweathermap.org/data/2.5/weather?q={city name},{state code},{country code}&appid={API key}
+  const onInputChange = (e: ChangeEvent<HTMLInputElement> ) => {
+    const value = e.target.value.trim()
+    setForm(value)
+    if (value === '') return
+    e.preventDefault()
+    getSearch(value)
+  }
+
+  // http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
 
 
 
