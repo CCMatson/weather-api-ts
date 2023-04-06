aea544efc29cdb5ea6abcae3c0ca5952af8e14de
CCMatson <catherine.matson@gmail.com>
2023-04-06 08:16:05 -0700

    fix typo

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,6 +1,6 @@
 import { ChangeEvent, useState } from 'react';
 import { option } from '../types';
-import OptionList from './optionLIst';
+import OptionList from './optionList';
 
 type Props = {
   form: string,
